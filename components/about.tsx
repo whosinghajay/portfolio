66a5135e7f4b38654e436bdca8ae20ebae2e92eb
Ajay Singh <whosinghajay@gmail.com>
2023-12-27 06:13:22 +0530

    intro and about section created

---
+++ components/about.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+
+export default function About() {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center sm:mb-40 leading-8"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+      id="about"
+    >
+      <SectionHeading>About Me</SectionHeading>
+      <p className="mb-3">
+        I am a <span className="font-semibold">Full Stack Developer</span>{" "}
+        specializing in the <span className="font-medium">MERN</span> stack,
+        with the expertise in{" "}
+        <span className="italic">React, Node, Express, MongoDB</span>. My
+        proficiency extends to technologies like{" "}
+        <span className="underline">Typescript</span> and{" "}
+        <span className="underline">Next.js</span>, enabling me to build robust
+        and scalable web applications.
+      </p>
+      <p className="mb-3">
+        I am driven by a perpetual curiosity and a hunger for learning{" "}
+        <span className="font-medium">new technologies</span>. Keeping up with
+        the latest industry trends is not just a habit but a commitment that
+        fuels my desire to deliver cutting-edge solutions.
+      </p>
+      <p className="mb-3">
+        My dedication to delivering{" "}
+        <span className="">high-quality software</span>, coupled with an
+        eagerness to take on challenges, defines my professional ethos. I thrive
+        in dynamic environments where <span className="">innovation</span> and{" "}
+        <span className="">adaptability</span> are valued.
+      </p>
+      <p className="">
+        I am open to new <span className="underline">opportunities</span> and{" "}
+        <span className="underline">collaborations</span> that allow me to
+        leverage my skills and contribute to exciting projects. If you're
+        looking for a Full Stack Developer who combines technical expertise with
+        a passion for innovation,{" "}
+        <span className="font-bold">let's connect</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ components/intro.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaLocationArrow } from "react-icons/fa6";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex itmes-center justify-center">
+        <div className="relative ">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src={
+                "https://media.istockphoto.com/id/958064228/photo/portrait-of-happy-young-man.jpg?s=612x612&w=is&k=20&c=aSA6MAeUA8LngFd23ELCLuyhA9et1uP4GHeySt_MEC0="
+              }
+              alt="Portrait"
+              height={"192"}
+              width={"192"}
+              quality={"95"}
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute text-4xl bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Ajay Singh.</span> I'm a{" "}
+        <span className="font-bold">Full Stack Developer</span> with{" "}
+        <span className="font-bold">years</span>{" "}
+        <span>of self-experience. I enjoy building</span>{" "}
+        <span className="italiic">sites & apps</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row gap-2 items-center justify-center"
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{
+          delay: 0.2,
+        }}
+      >
+        <Link
+          href={"#contact"}
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <Link
+          href={""}
+          className="group bg-rose-500 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-rose-550 active:scale-105 transition"
+          target="_blank"
+        >
+          My Resume
+          <FaLocationArrow className="opacity-80 group-hover:translate-x-1 transition" />
+        </Link>
+        <Link
+          href={"https://www.linkedin.com/in/ajayyssingh"}
+          className="p-2 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-[1.25] active:scale-105 transition"
+          target="_blank"
+        >
+          <FaLinkedin size={"30"} />
+        </Link>
+        <Link
+          href={"https://www.instagram.com/ajayyssingh/"}
+          className="p-2 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-[1.25] active:scale-105 transition"
+          target="_blank"
+        >
+          <FaInstagram size={"30"} />
+        </Link>
+      </motion.div>
+    </section>
+  );
+}
