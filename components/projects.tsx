e1c8b187070dce75b6a8aa82fc4b378dca682c5b
Ajay Singh <whosinghajay@gmail.com>
2023-12-29 22:47:36 +0530

    function name changed from projects to  Projects as it was showing the error for naming convention error

--- components/projects.tsx
+++ components/projects.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import Project from "./project";
 import SectionHeading from "./section-heading";
 
-export default function projects() {
+export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
   return (
