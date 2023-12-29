a564fa85d9af74d95223b317437746d2210ba833
Ajay Singh <whosinghajay@gmail.com>
2023-12-29 17:05:31 +0530

    contact section complete and started the footer section

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,43 @@
 "use server";
 
+import { getErrorMessage, validatString } from "@/lib/utils";
+import { Resend } from "resend";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
-  console.log("Running on the server");
-  console.log(formData.get("senderEmail"));
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  //simple server side validation
+  if (!validatString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validatString(message, 500)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  try {
+    await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "whosinghajay@gmail.com",
+      subject: "Message from Contact form",
+      reply_to: senderEmail as string,
+      //   text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 };
--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -57,17 +57,11 @@ export default function Contact() {
           className="h-52 my-3 rounded-lg borderBlack p-4"
           name="message"
           required
-          maxLength={500}
+          maxLength={5000}
           placeholder="Your Message"
         />
-        <button
-          type="submit"
-          className=" group flex items-center justify-center gap-2 h-[3rem] w-full sm:w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
-        >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-        </button>
+       <SubmitBtn/>
       </form>
     </motion.section>
   );
-}
+} 
