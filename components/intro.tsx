b9e91390caf69fab0c8d6b3ffff5b3eb8719cdbf
Ajay Singh <whosinghajay@gmail.com>
2023-12-30 20:28:35 +0530

    intro and skills section's mobile changed

--- components/intro.tsx
+++ components/intro.tsx
@@ -59,7 +59,7 @@ export default function Intro() {
       </div>
 
       <motion.p
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
--- components/skills.tsx
+++ components/skills.tsx
@@ -29,7 +29,7 @@ export default function Skills() {
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
     >
       <SectionHeading>My Skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+      <ul className="flex flex-wrap justify-center gap-2 text-md sm:text-lg text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li
             className="bg-white border border-black/[0.1] rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
